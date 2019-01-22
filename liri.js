53ffa46419f91936c48e5f7b6ffb2de2323acabb
KarateMaltLiquor <43533814+karatemaltliquor@users.noreply.github.com>
2019-01-21 19:16:36 -0500

    working spotify query

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var moment = require('moment');
 moment().format();
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var axios = require("axios");
 var omdbApi = require('omdb-client');
 var bandsintown = require('bandsintown');
@@ -26,6 +26,26 @@ inquirer.prompt([
 
     ]).then(function(user) {
         
-        console.log("OK" + user.commando + user.searcher.split(' ').join('+'));
+        // console.log("OK" + user.commando + user.searcher.split(' ').join('+'));
+
+        var spotify = new Spotify({
+            id: process.env.SPOTIFY_ID,
+            secret: process.env.SPOTIFY_SECRET,
+          });
+
+        if (user.commando === "spotify-this-song") {
+            spotify.search({ type: 'track', query: user.searcher, limit: 1 }, function(err, data) {
+                if (err) {
+                  return console.log('Error occurred: ' + err);
+                }
+               
+            //   console log of Spotify info             
+              console.log("Artists : " + data.tracks.items[0].album.artists[0].name); 
+              console.log("Song Title : " + data.tracks.items[0].name);
+              console.log("Link to Song on Spotify : " + data.tracks.items[0].album.artists[0].external_urls.spotify);
+              console.log("Album Title : " + data.tracks.items[0].album.name); 
+              });
+        }
+            
 
     });
